9aab404d093c14b712e6e95519cb241586dd9342
LazeeCoder <svenable88@yahoo.com>
2014-06-17 00:07:58 -0400

    First Checkin

---
+++ BunnyFarm/Bunny.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyFarm
+{
+    public class Bunny
+    {
+        public string Name { get; set; }
+     
+        public Bunny()
+        {
+            Name = "Frodo";
+            Age = 1;
+        }
+
+        public int Age { get; set; }
+    }
+}
---
+++ BunnyFarmTests/BunnyTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BunnyFarm;
+
+namespace BunnyFarmTests
+{
+    [TestClass]
+    public class BunnyTests
+    {
+        private Bunny myFirstBunny;
+
+        [TestInitialize]
+        public void InitializeBunny()
+        {
+            myFirstBunny = new Bunny();
+        }
+        
+        [TestMethod]
+        public void CanCreateBunny()
+        {
+            Assert.IsNotNull(myFirstBunny);
+        }
+
+        [TestMethod]
+        public void MyFirstBunnyHasAName()
+        {
+            Assert.IsNotNull(myFirstBunny.Name);
+        }
+
+
+        [TestMethod]
+        public void MyFirstBunnyHasAnAge()
+        {
+            Assert.IsInstanceOfType(myFirstBunny.Age, typeof(int));
+        }
+
+        [TestMethod]
+        public void MyFirstBunnyIsOlderThanZero()
+        {
+            Assert.IsTrue(myFirstBunny.Age > 0);
+        }
+
+    }
+}
