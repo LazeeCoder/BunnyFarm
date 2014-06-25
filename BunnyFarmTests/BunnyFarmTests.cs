d5a1857b412d1aa51d499d7d45c758dbb112b620
LazeeCoder <svenable88@yahoo.com>
2014-06-25 01:28:43 -0400

    BunnyFarm First Test

---
+++ BunnyFarmTests/BunnyFarmTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BunnyFarm;
+using System.Collections.Generic;
+using Moq;
+
+namespace BunnyFarmTests
+{
+    [TestClass]
+    public class BunnyFarmTests
+    {
+        private List<IBunny> mockBunnies;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockBunnies = new List<IBunny>();
+
+            mockBunnies.Add(new Bunny { Age = 1, Color = BunnyColor.SPOTTED, Sex = BunnySex.MALE, IsRadioActive = false, Name = "Test Male" });
+            mockBunnies.Add(new Bunny { Age = 1, Color = BunnyColor.BLACK, Sex = BunnySex.FEMALE, IsRadioActive = false, Name = "Test Female" });
+        }
+
+
+        [TestMethod]
+        public void AMaleAndFemaleBunnyWillCreateANewBunnyUponAging()
+        {
+            Farm myFarm = new Farm(mockBunnies);
+
+            myFarm.AgeBunnies();
+
+            Assert.AreEqual(3, myFarm.Bunnies.Count);
+
+
+        }
+    }
+}
